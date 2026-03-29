34766c7535a8477df18a37c699ee56bca3304d48
DexterWard <benon.f@gmail.com>
2026-03-29 16:12:04 +0200

    New changes

--- java-maven/src/main/java/com/App.java
+++ java-maven/src/main/java/com/App.java
@@ -1,4 +1,4 @@
-package com.dexter;
+package com.cloudwithvarjosh;
 
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // 2) Using System.out instead of logger (Maintainability)
-        System.out.println("Starting DevSecOps Demo...");
+        System.out.println("Starting CVWJ DevSecOps Demo...");
 
         // 3) Scanner resource leak + wrong string comparison (two findings)
         String username = "guest"; // safe fallback for Docker
@@ -48,12 +48,12 @@ public class App {
             os.close();
         });
 
-        System.out.println("Steady app running on port " + port);
+        System.out.println("CVWJ app running on port " + port);
         server.start();
     }
 
     // simple helper method for tests
     public static String brandHtml() {
-        return "<h1>Ci/CD pipeline example</h1><p>Simple DevSecOps Demo App</p>";
+        return "<h1>Cloud With VarJosh</h1><p>Simple DevSecOps Demo App</p>";
     }
 }
